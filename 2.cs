e7b82510cc71cfd894dceb1ecc592e332cb75f7c
rinel4ik740 <rzinnatov@litsey7.com>
2025-05-13 18:39:14 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp5
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "C:\\Users\\pc15\\source\\repos\\ConsoleApp5\\App.config\\userInput.txt";
+        AnalyzeFile(path);
+    }
+
+    static void AnalyzeFile(string path)
+    {
+
+        int lineCount = 0;
+        int wordCount = 0;
+        int charCount = 0;
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineCount++;
+                charCount += line.Length;
+                wordCount += line.Split(new[] { ' ' }).Length;
+            }
+        }
+
+        Console.WriteLine($"Строк: {lineCount}");
+        Console.WriteLine($"Слов: {wordCount}");
+        Console.WriteLine($"Символов: {charCount}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\pc15\\source\\repos\\ConsoleApp6\\App.config\\userInput.txt";
+            SaveUserInputToFile(path);
+        }
+
+        static void SaveUserInputToFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                string input;
+                do
+                {
+                    Console.Write("Введите строку (exit - завершить): ");
+                    input = Console.ReadLine();
+
+                    if (input != "exit")
+                    {
+                        sw.WriteLine(input);
+                    }
+                } while (input != "exit");
+            }
+
+            Console.WriteLine($"Данные сохранены в файл: {path}");
+        }
+    }
+}
